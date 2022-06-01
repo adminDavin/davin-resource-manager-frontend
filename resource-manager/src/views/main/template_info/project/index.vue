149574be96e350877cd18f30a1a413cb5cfbc125
davin <18758268513@163.com>
2022-06-01 11:50:52 +0800

    fixed

--- resource-manager/src/views/main/template_info/project/index.vue
+++ resource-manager/src/views/main/template_info/project/index.vue
@@ -3,7 +3,7 @@
     v-loading="dialogTableVisible"
     element-loading-text="项目正在创建中, 请等待..."
   >
-    <el-col :xs="24" :sm="14" :md="12"  :lg="4">
+    <el-col :xs="24" :sm="14" :md="12" :lg="4">
       <div style="text-align: center; margin-top: 20px; margin-bottom: 30px">
         <strong :style="`font-size: 30px;`">设计项目模版</strong>
       </div>
@@ -12,9 +12,17 @@
         style="margin-top: 15px; object-fit: contain"
       ></el-image>
     </el-col>
-    <el-col :xs="24" :sm="14" :md="12" :lg="8">
+    <el-col :xs="24" :sm="14" :md="12" :lg="10">
       <div style="margin: 30px">
-        <el-input v-model="projectName" placeholder="项目名称" />
+        <el-input
+          v-model="projectName"
+          placeholder="类型+组号+序号-开发商-地点+项目名"
+        />
+        <el-alert type="info" show-icon :closable="false">
+          <p>类型：拿地简写ND，生产项目简写SC</p>
+          <p>组号：一所即1</p>
+          <p>示例：以1组第一块拿地为例，ND1001-金地-青岛市XX项目</p>
+        </el-alert>
       </div>
       <div style="margin: 30px">
         <el-button type="primary" @click="createDesignProject" round>
@@ -22,7 +30,7 @@
         </el-button>
       </div>
     </el-col>
-    <el-col :xs="0" :sm="0" :md="0" :lg="12">
+    <el-col :xs="0" :sm="0" :md="0" :lg="10">
       <el-card shadow="never">
         <h4>模版结构</h4>
         <el-tree
@@ -38,18 +46,18 @@
     </el-col>
   </el-row>
   <el-dialog v-model="dialogVisible" title="请再此确认账号密码" width="30%">
-      <span>请再此确认账号密码</span>
-      <el-input v-model="nextcloudUid" placeholder="账号" />
-      <el-input v-model="nextcloudPass" placeholder="密码" />
-      <template #footer>
-        <span class="dialog-footer">
-          <el-button @click="dialogVisible = false">取消</el-button>
-          <el-button type="primary" @click="dialogVisible = false"
-            >确定</el-button
-          >
-        </span>
-      </template>
-    </el-dialog>
+    <span>请再此确认账号密码</span>
+    <el-input v-model="nextcloudUid" placeholder="账号" />
+    <el-input v-model="nextcloudPass" placeholder="密码" />
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >确定</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
 </template>
 <script lang="ts">
 import { defineComponent, ref, onMounted } from "vue";
@@ -58,7 +66,7 @@ import common_utils from "@/utils/common_utils";
 import alert_utils from "@/utils/alert_utils";
 import type { ElTree } from "element-plus";
 import { ElMessage, ElMessageBox } from "element-plus";
-import Base64 from 'base-64';
+import Base64 from "base-64";
 import {
   projectTemplate,
   projectCreate,
